6df2561a35b7a31edd034f9d54772d34061cd421
Heather Shelley <heather@Heathers-iMac.local>
2011-12-28 21:01:46 -0700

    finished chapter 5's basic interface

--- wizards-adventure.rkt
+++ wizards-adventure.rkt
@@ -9,6 +9,45 @@
                 (garden (living-room east door))
                 (attic (living-room downstairs ladder))))
 
+(define objects '(whiskey bucket frog chain))
+
+(define location 'living-room)
+
+(define (walk direction)
+  (define (has-direction? l)
+    (eq? direction (second l)))
+  (define next
+    (findf has-direction? (rest (assoc location edges))))
+  (if next
+      (begin (set! location (first next))
+             (look location))
+      '(You can't go that direction)))
+
+(define object-locations '((whiskey living-room)
+                           (bucket living-room)
+                           (chain garden)
+                           (frog garden)))
+
+(define (objects-at loc objs obj-locs)
+  (define (at-location? obj)
+    (eq? loc (second (assoc obj obj-locs))))
+  (filter at-location? objs))
+
+(define (describe-objects loc objs objs-loc)
+  (define (describe-object obj)
+    `(You see a ,obj on the floor.))
+  (append-map describe-object (objects-at loc objs objs-loc)))
+
+(define (pickup object)
+  (cond 
+    [(member object (objects-at location objects object-locations))
+     (set! object-locations (cons (list object 'body) object-locations))
+     `(You are now carrying the ,object)]
+    [else '(You cannot get that)]))
+
+(define (inventory)
+  (cons 'items: (objects-at 'body objects object-locations)))
+
 (define (describe-location location nodes)
   (first (rest (assoc location nodes))))
 
@@ -17,3 +56,8 @@
 
 (define (describe-paths location edges)
   (append-map describe-path (rest (assoc location edges))))
+
+(define (look loc)
+  (append (describe-location loc nodes)
+          (describe-paths loc edges)
+          (describe-objects loc objects object-locations)))
